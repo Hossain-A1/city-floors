51b29b05ba0c218cb18edb2b91954d37c8e2da48
Hossain-A1 <mrhossainahmed7@gmail.com>
2024-04-15 13:11:11 +0300

    login logic added

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,18 +5,45 @@ import useLoginUser from "@/hooks/useLoginUser";
 import Link from "next/link";
 import Button from "@/components/ui/Button";
 import Image from "next/image";
-
+import { useRouter } from "next/navigation";
+import { useCallback, useState } from "react";
+import { userLoginPost } from "@/lib/userLoginPost";
+import toast from "react-hot-toast";
+interface SignUpFormData {
+  email: string;
+  password: string;
+}
 const LoginPage = () => {
- 
-  const {
-    handleLogin,
-    loginWithFB,
-    loginWithGoogle,
-    emailHaldleChange,
-    passHanldeChange,
-    email,
-    password,
-  } = useLoginUser();
+  const { loginWithFB, loginWithGoogle } = useLoginUser();
+  const [formData, setFormData] = useState<SignUpFormData>({
+    email: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const router = useRouter();
+  const handleSubmit = useCallback(
+    async (e: React.SyntheticEvent) => {
+      e.preventDefault();
+      setLoading(true);
+
+      const data = await userLoginPost("/api/auth/login", formData);
+
+      if (data) {
+        setLoading(false);
+        setFormData({
+          email: "",
+          password: "",
+        });
+        toast.success("Register successfull");
+        router.push("/");
+      } else {
+        setLoading(false);
+      }
+    },
+    [formData, router]
+  );
 
   return (
     <main className='sp mt-5 container min-h-screen  grid lg:grid-cols-2 grid-cols-1 gap-5'>
@@ -46,29 +73,37 @@ const LoginPage = () => {
           <hr className='border border-blue w-12 lg:w-20' />
         </div>
         <div className=' w-full lg:w-[21rem] xl:w-[30rem]'>
-          <form onSubmit={handleLogin} className='flex flex-col gap-3 '>
+          <form onSubmit={handleSubmit} className='flex flex-col gap-3 '>
             <input
               className='py-2.5 eq px-4  rounded outline-none border bg-light focus:border-blue shadow-sm'
               type='email'
               placeholder='Email'
-              value={email}
-              onChange={emailHaldleChange}
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
               required
             />
             <input
               className='py-2.5 eq px-4 rounded outline-none border bg-light focus:border-blue shadow-sm'
               type='password'
               placeholder='Password'
-              value={password}
-              onChange={passHanldeChange}
+              value={formData.password}
+              onChange={(e) =>
+                setFormData({ ...formData, password: e.target.value })
+              }
               required
             />
-            <Button type='submit' variant='secondary' >Login</Button> 
+            <Button type='submit' variant='secondary' isLoading={loading}>
+              Login
+            </Button>
             <hr />
-           <div className="flex flex-col items-center gap-3">
-           <strong>{`Don't have an account?`}</strong>
-            <Link href='signup' className="text-orange text-sm">Sign up</Link>
-           </div>
+            <div className='flex flex-col items-center gap-3'>
+              <strong>{`Don't have an account?`}</strong>
+              <Link href='signup' className='text-orange text-sm'>
+                Sign up
+              </Link>
+            </div>
           </form>
         </div>
       </div>
@@ -87,9 +122,9 @@ const LoginPage = () => {
         <div className='flex flex-col gap-5 items-center'>
           <h2>Login to CityFloors</h2>
           <small>You can access our all features after log in.</small>
-          <div className="flex items-center gap-2">
-          <span className='h-4 w-14 bg-blue p-2 rounded-full'></span>
-          <span className='h-4 w-4 bg-orange p-2 rounded-full'></span>
+          <div className='flex items-center gap-2'>
+            <span className='h-4 w-14 bg-blue p-2 rounded-full'></span>
+            <span className='h-4 w-4 bg-orange p-2 rounded-full'></span>
           </div>
         </div>
       </div>
